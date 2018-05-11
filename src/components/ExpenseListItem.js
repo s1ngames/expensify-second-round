d01b2914187874a1197b5067cdfc47124f81580d
Nir Beck <nir@beckermus.com>
2018-05-11 19:45:27 +0300

    added currency and date normalize

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,14 +2,55 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { removeExpense } from '../actions/expenses';
-const ExpenseListItem = (props) => (
-    <div>
-        <Link to={`/edit/${props.expense.id}`}>
-            <h3>{props.expense.description}</h3>
+import moment from 'moment';
+import numeral from 'numeral';
+
+class ExpenseListItem extends React.Component {
+    constructor(props){
+        super(props);
+       
+        if (numeral.locales['user-locale'] === undefined) {
+            numeral.register('locale', 'user-locale', {
+                delimiters: {
+                    thousands: ',',
+                    decimal: '.'
+                },
+                abbreviations: {
+                    thousand: 'k',
+                    million: 'm',
+                    billion: 'b',
+                    trillion: 't'
+                },
+                ordinal : function (number) {
+                    return number === 1 ? 'er' : 'ème';
+                },
+                currency: {
+                    symbol: '₪'
+                }
+            });
+            numeral.locale('user-locale');
+          };
+        
+        
+    }
+
+    render(){
+        return(
+            <div>
+        <Link to={`/edit/${this.props.expense.id}`}>
+            <h3>{this.props.expense.description}</h3>
         </Link>
-        <p>{props.expense.amount} - {props.expense.createdAt}</p>
+        <p>
+        {numeral(this.props.expense.amount / 100).format('0,0.00$')}
+         - 
+        {moment(this.props.expense.createdAt).format('DD/MM/YYYY')}
+         
+        </p>
     </div>
-);
+        );
+    }
+}
+
 
 // const mapStateToProps = (state) => {
 //     return{
